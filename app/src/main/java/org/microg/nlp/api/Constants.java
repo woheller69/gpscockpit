6effe8347d3508fcb58adab74ded8954e814429e
woheller69 <wolfgang.heller@web.de>
2021-11-21 22:28:33 +0100

    remove unused stuff 4

--- app/src/main/java/org/microg/nlp/api/Constants.java
+++
@@ -1,8 +0,0 @@
-package org.microg.nlp.api;
-
-public class Constants {
-
-  public static final String ACTION_LOCATION_BACKEND = "org.microg.nlp.LOCATION_BACKEND";
-  public static final String METADATA_BACKEND_INIT_ACTIVITY =
-      "org.microg.nlp.BACKEND_INIT_ACTIVITY";
-}
